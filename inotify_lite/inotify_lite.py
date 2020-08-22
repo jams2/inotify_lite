a589131c8bcafdfb5f266d61374d59ed37af10f0
jams2 <public@elysee-munn.family>
2020-08-22 13:11:24 +0100

    adds subdirectory registering to TreeWalker

--- inotify_lite/inotify_lite.py
+++ inotify_lite/inotify_lite.py
@@ -19,7 +19,7 @@ from ctypes import (
     create_string_buffer,
 )
 from string import Template
-from typing import Callable, Any, Dict
+from typing import Callable, Any, Dict, Set, List
 from struct import unpack, calcsize
 from collections import namedtuple
 
@@ -48,32 +48,45 @@ class INFlags(enum.IntFlag):
     """ See inotify_add_watch(2), <sys/inotify.h>, <bits/inotify.h>.
     """
 
-    NOFLAGS = 0x0
-    NONBLOCK = 0x800
-    CLOEXEC = 0x80000
-    ACCESS = 0x00000001
-    MODIFY = 0x00000002
-    ATTRIB = 0x00000004
-    CLOSE_WRITE = 0x00000008
-    CLOSE_NOWRITE = 0x00000010
-    OPEN = 0x00000020
-    MOVED_FROM = 0x00000040
-    MOVED_TO = 0x00000080
-    CREATE = 0x00000100
-    DELETE = 0x00000200
-    DELETE_SELF = 0x00000400
-    MOVE_SELF = 0x00000800
-    UNMOUNT = 0x00002000
-    Q_OVERFLOW = 0x00004000
-    IGNORED = 0x00008000
-    CLOSE = CLOSE_WRITE | CLOSE_NOWRITE
-    MOVE = MOVED_FROM | MOVED_TO
-    ONLYDIR = 0x01000000
-    DONT_FOLLOW = 0x02000000
-    EXCL_UNLINK = 0x04000000
-    MASK_ADD = 0x20000000
-    ISDIR = 0x40000000
-    ONESHOT = 0x80000000
+    NO_FLAGS = 0x0
+    CLOEXEC = 0x00080000
+    NONBLOCK = 0x00000800
+
+    #  Supported events suitable for MASK parameter of INOTIFY_ADD_WATCH.
+    ACCESS = 0x00000001  #  File was accessed.
+    MODIFY = 0x00000002  #  File was modified.
+    ATTRIB = 0x00000004  #  Metadata changed.
+    CLOSE_WRITE = 0x00000008  #  Writable file was closed.
+    CLOSE_NOWRITE = 0x00000010  #  Unwritable file closed.
+    CLOSE = CLOSE_WRITE | CLOSE_NOWRITE  #  Close.
+    OPEN = 0x00000020  #  File was opened.
+    MOVED_FROM = 0x00000040  #  File was moved from X.
+    MOVED_TO = 0x00000080  #  File was moved to Y.
+    MOVE = MOVED_FROM | MOVED_TO  #  Moves.
+    CREATE = 0x00000100  #  Subfile was created.
+    DELETE = 0x00000200  #  Subfile was deleted.
+    DELETE_SELF = 0x00000400  #  Self was deleted.
+    MOVE_SELF = 0x00000800  #  Self was moved.
+
+    #  Events sent by the kernel.
+    UNMOUNT = 0x00002000  #  Backing fs was unmounted.
+    Q_OVERFLOW = 0x00004000  #  Event queued overflowed.
+    IGNORED = 0x00008000  #  File was ignored.
+
+    #  Helper events.
+    CLOSE = CLOSE_WRITE | CLOSE_NOWRITE  #  Close.
+    MOVE = MOVED_FROM | MOVED_TO  #  Moves.
+
+    #  Special flags.
+    ONLYDIR = 0x01000000  #  Only watch the path if it is a directory.
+    DONT_FOLLOW = 0x02000000  #  Do not follow a sym link.
+    EXCL_UNLINK = 0x04000000  #  Exclude events on unlinked objects.
+    MASK_CREATE = 0x10000000  #  Only create watches.
+    MASK_ADD = 0x20000000  # Add to the mask of an already existing watch.
+    ISDIR = 0x40000000  #  Event occurred against dir.
+    ONESHOT = 0x80000000  #  Only send event once.
+
+    #  All events which a program can wait on.
     ALL_EVENTS = (
         ACCESS
         | MODIFY
@@ -105,37 +118,38 @@ class Inotify:
         callback: Callable[[Event], Any],
         *files: str,
         blocking: bool = True,
-        flags: INFlags = INFlags.NOFLAGS,
+        watch_flags: INFlags = INFlags.NO_FLAGS,
     ):
-        self.inotify_fd = self._blocking() if blocking else self._non_blocking()
+        self.inotify_fd = inotify_init1(
+            INFlags.NO_FLAGS if blocking else INFlags.NONBLOCK
+        )
         if self.inotify_fd < 0:
             raise OSError(os.strerror(get_errno()))
         self.callback = callback
-        self.flags = flags
+        self.watch_flags = watch_flags
         self.watch_fds: Dict[int, str] = {}
-        self.files = files
-        self._add_watches()
+        self.files: Set[str] = set(files)
+        for fname in self.files:
+            self._add_watch(fname, INFlags.MASK_CREATE)
 
-    def _add_watches(self):
-        for fname in map(os.path.expanduser, self.files):
-            if not os.path.exists(fname):
-                raise FileNotFoundError(fname)
-            as_bytes = fname.encode("utf-8")
-            watch_fd = inotify_add_watch(
-                c_int(self.inotify_fd), c_char_p(as_bytes), c_uint32(self.flags),
-            )
-            if watch_fd < 0:
-                err = os.strerror(get_errno())
-                raise OSError(err)
-            self.watch_fds[watch_fd] = fname
-
-    @staticmethod
-    def _blocking():
-        return inotify_init()
+    def _add_watch(self, fname: str, add_flags: INFlags = INFlags.NO_FLAGS) -> None:
+        if not os.path.exists(fname):
+            raise FileNotFoundError(fname)
+        watch_fd = inotify_add_watch(
+            c_int(self.inotify_fd),
+            c_char_p(fname.encode("utf-8")),
+            c_uint32(self.watch_flags | add_flags),
+        )
+        if watch_fd < 0:
+            err = os.strerror(get_errno())
+            raise OSError(err)
+        self.watch_fds[watch_fd] = fname
 
     @staticmethod
-    def _non_blocking():
-        return inotify_init1(INFlags.NONBLOCK)
+    def str_from_bytes(byte_obj: bytes) -> str:
+        """ Convert null terminated bytes to Python string.
+        """
+        return byte_obj.decode().split("\x00")[0]
 
     def _handle_event(self, event: Event) -> None:
         self.callback(event)
@@ -180,12 +194,51 @@ class TreeWatcher(Inotify):
         self,
         callback: Callable[[Event], Any],
         *dirs: str,
+        watch_subdirs: bool = True,
         blocking: bool = True,
-        flags: INFlags = INFlags.ALL_EVENTS,
+        watch_flags: INFlags = INFlags.ALL_EVENTS,
     ):
+        self.watch_subdirs = watch_subdirs
+        self.mv_events: Dict[int, Event] = {}
+        dir_paths = [os.path.abspath(os.path.expanduser(x)) for x in dirs]
+        all_dirs = self._walk_subdirs(dir_paths) if watch_subdirs else dir_paths
         super().__init__(
-            callback, *dirs, blocking=blocking, flags=flags | INFlags.ONLYDIR
+            callback,
+            *all_dirs,
+            blocking=blocking,
+            watch_flags=watch_flags | INFlags.ONLYDIR,
         )
+
+    def _walk_subdirs(self, dirs: List[str]) -> List[str]:
+        if not dirs:
+            return []
+        subdirs = []
+        for dirname in dirs:
+            subdirs += [x for x in os.listdir(dirname) if os.path.isdir(x)]
+        return dirs + self._walk_subdirs(subdirs)
+
+    def _handle_event(self, event: Event) -> None:
+        """ As we may be watching for changes in all subdirectories,
+        there are a few special cases to watch out for; new subdirectories,
+        deleted subdirectories, and moved subdirectories.
+        """
+        if event.mask & INFlags.ISDIR:
+            if event.mask & INFlags.CREATE:
+                print("New subdirectory: ", end="")
+                print(self.watch_fds[event.wd] + "/" + self.str_from_bytes(event.name))
+            elif event.mask & INFlags.DELETE:
+                print("Subdirectory deleted: ", end="")
+                print(self.watch_fds[event.wd] + "/" + self.str_from_bytes(event.name))
+            elif event.mask & (INFlags.MOVED_FROM | INFlags.MOVED_TO):
+                print("Subdirectory moved: ", end="")
+                print(self.watch_fds[event.wd] + "/" + self.str_from_bytes(event.name))
+        if event.mask & INFlags.DELETE_SELF:
+            # A watched directory was deleted.
+            pass
+        if event.mask & INFlags.MOVE_SELF:
+            # A watched directory was moved.
+            pass
+        self.callback(event)
 
 
 class FileWatcher(Inotify):
